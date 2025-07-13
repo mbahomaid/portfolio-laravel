b588ff7b0af953c98f9a6a61ec3d228ddadb7554
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-07-14 01:56:42 +0300

    18. Customizing Reset Password View

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,151 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
+    <meta name="author" content="NobleUI">
+    <meta name="keywords"
+        content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <title>NobleUI - HTML Bootstrap 5 Admin Dashboard Template</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- End fonts -->
+
+    <!-- core:css -->
+    <link rel="stylesheet" href="{{ asset('../../../backend/assets/vendors/core/core.css')  }} ">
+    <!-- endinject -->
+
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('../../../backend/assets/fonts/feather-font/css/iconfont.css')  }} ">
+    <link rel="stylesheet" href="{{ asset('../../../backend/assets/vendors/flag-icon-css/css/flag-icon.min.css')  }}">
+    <!-- endinject -->
+
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{ asset('../../../backend/assets/css/demo2/style.css')  }}">
+    <!-- End layout styles -->
+
+    <link rel="shortcut icon" href="{{ asset('../../../backend/assets/images/favicon.png')  }}" />
+    <style>
+        .auth-login-side-wrapper {
+            background-image: url('../../../backend/assets/images/login.png');
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <div class="page-wrapper full-page">
+            <div class="page-content d-flex align-items-center justify-content-center">
+
+                <div class="row w-100 mx-0 auth-page">
+                    <div class="col-md-8 col-xl-6 mx-auto">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col-md-4 pe-md-0">
+                                    <div class="auth-login-side-wrapper">
+
+                                    </div>
+                                </div>
+                                <div class="col-md-8 ps-md-0">
+                                    <div class="auth-form-wrapper px-4 py-5">
+                                        <a href="#" class="noble-ui-logo logo-light d-block mb-2">Reset
+                                            <span>Password</span></a>
+                                        <h5 class="text-muted fw-normal mb-4">Enter a new password.</h5>
+
+                                        @if(session('status'))
+                                            <div class="alert alert-success" role="alert">
+                                                {{ session('status') }}
+                                            </div>
+                                        @endif
+
+                                        <form class="forms-sample" method="POST" action="{{ route('password.store') }}">
+                                            @csrf
+                                            <!-- Password Reset Token -->
+                                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                            <div class="mb-3">
+                                                <label for="userEmail" class="form-label">Email</label>
+                                                <input type="text"
+                                                    class="form-control @error('email') is-invalid @enderror"
+                                                    name="email" value="{{old('email', $request->email) }}">
+
+                                                @error('email')
+                                                    <span class="text-danger"> {{ $message }} </span>
+                                                @enderror
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label for="userPassword" class="form-label">New Password</label>
+                                                <input type="password"
+                                                    class="form-control @error('password') is-invalid @enderror"
+                                                    name="password" autocomplete="current-password"
+                                                    placeholder="New Password">
+
+                                                @error('password')
+                                                    <span class="text-danger"> {{ $message }} </span>
+                                                @enderror
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="password_confirmation" class="form-label">Confirm New
+                                                    Password</label>
+                                                <input type="password"
+                                                    class="form-control @error('password_confirmation') is-invalid @enderror"
+                                                    name="password_confirmation" autocomplete="current-password"
+                                                    placeholder="Confirm New Password">
+
+                                                @error('password_confirmation')
+                                                    <span class="text-danger"> {{ $message }} </span>
+                                                @enderror
+                                            </div>
+
+                                            <div>
+                                                <button type="submit"
+                                                    class="btn btn-outline-primary btn-icon-text mb-2 mb-md-0">
+                                                    Reset Password
+                                                </button>
+                                            </div>
+
+
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+    <!-- core:js -->
+    <script src="{{ asset('../../../backend/assets/vendors/core/core.js')  }}"></script>
+    <!-- endinject -->
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+    <!-- inject:js -->
+    <script src="{{ asset('../../../backend/assets/vendors/feather-icons/feather.min.js')  }}"></script>
+    <script src="{{ asset('../../../backend/assets/js/template.js')  }}"></script>
+    <!-- endinject -->
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+    <!-- Custom js for this page -->
+    <!-- End custom js for this page -->
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</body>
+
+</html>
