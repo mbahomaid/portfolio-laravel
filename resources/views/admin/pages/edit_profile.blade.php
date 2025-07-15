448ce318ab979e8ba277c70042d2a45961c1c839
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-07-16 00:41:28 +0300

    20. Admin Edit Profile Part 2

--- resources/views/admin/pages/edit_profile.blade.php
+++ resources/views/admin/pages/edit_profile.blade.php
@@ -1,5 +1,49 @@
 @extends('admin.admin_dashboard')
 @section('admin')
-    <h2>Edit Admin Profile</h2>
+    <div class="col-md-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+
+                <h6 class="card-title">Edit Profile</h6>
+
+                <form class="forms-sample">
+                    <div class="row mb-3">
+                        <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Username</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" value="{{ $admin->username }}" placeholder="Username">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Email</label>
+                        <div class="col-sm-9">
+                            <input type="email" class="form-control" value="{{ $admin->email }}" autocomplete="off"
+                                placeholder="Email">
+                        </div>
+                    </div>
+
+
+                    <div class="row mb-3">
+                        <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Profile Photo</label>
+                        <div class="col-sm-9">
+                            <input class="form-control" type="file" id="formFile">
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="row mb-3">
+                        <label for="exampleInputEmail2" class="col-sm-3 col-form-label"></label>
+                        <div class="col-sm-9">
+                            <img src="{{ asset('uploads/no-img-avatar.png') }}" alt="" style="width:90px; height: 90px;">
+
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-secondary">Update</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
 
 @endsection
